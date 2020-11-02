124004636744daa7625cbce094d7fc873fbf326c
Monoloki <62743722+Monoloki@users.noreply.github.com>
2020-11-02 18:48:23 +0100

    123123

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,13 @@
+﻿namespace GradeBook.GradeBooks
+{
+	public class StandardGradeBook: BaseGradeBook
+	{
+		public StandardGradeBook(string name): base(name)
+		{
+			Type = Enums.GradeBookType.Standard;
+		}
+
+		
+	}
+
+}
